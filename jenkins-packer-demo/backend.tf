e7bf0f65ee995a6da3c8b8837e0c13523679918e
arigsela <arigsela@gmail.com>
2022-03-10 12:26:32 -0500

    Updating bucket

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-state-ubjf343"
-#    key    = "terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-ubjf343"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
